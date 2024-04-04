5500e8634befb5c63d52083a38a3397c7f274fd4
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2024-04-04 08:23:00 +0700

    Revisi alamat

--- SayurSquare/ContactDatabase.swift
+++ SayurSquare/ContactDatabase.swift
@@ -12,9 +12,9 @@ struct Contact {
 
 // Database kontak
 let contactDatabase: [Contact] = [
-    Contact(name: "dr. Mega", phoneNumber: "08143234534", latitude: -6.30083, longitude: 106.64963, address: "Stasiun Karet"),
-    Contact(name: "dr. Anwar", phoneNumber: "110", latitude: -6.30355, longitude: 106.65551, address: "AEON gate 2"),
-    Contact(name: "dr. Saipul", phoneNumber: "113", latitude: -6.30580, longitude: 106.65345, address: "GOP 9 Apple DEV"),
+    Contact(name: "dr. Mega", phoneNumber: "08143234534", latitude: -6.30083, longitude: 106.64963, address: "JL. BSD GRAND BOULEVARD"),
+    Contact(name: "dr. Anwar", phoneNumber: "110", latitude: -6.30355, longitude: 106.65551, address: "JL. DAMAI FORESTA"),
+    Contact(name: "dr. Saipul", phoneNumber: "113", latitude: -6.30580, longitude: 106.65345, address: "JL. SAMPORA RAYA"),
     // Tambahkan kontak lain jika diperlukan
     
 //    Contact(name: "dr. Mega", phoneNumber: "08143234534", latitude: 37.33683, longitude: -122.01218, address: "Stasiun Karet"),
